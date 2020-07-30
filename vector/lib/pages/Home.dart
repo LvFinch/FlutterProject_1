3dfd2b69a5e96a38169c571a53a4432b67c1a245
Akko <33564554+LvFinch@users.noreply.github.com>
2020-07-30 19:02:18 +0800

    Create Home.dart

---
+++ vector/lib/pages/Home.dart
@@ -0,0 +1,24 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-07-30 18:40:52
+ * @LastEditTime: 2020-07-30 18:51:05
+ * @LastEditors: Akko
+ * @Description: home
+ * @FilePath: /vector/lib/pages/home.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
