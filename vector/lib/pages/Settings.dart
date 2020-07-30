e274986595f53e662707a8aba5cc909b59741558
Akko <33564554+LvFinch@users.noreply.github.com>
2020-07-30 19:02:21 +0800

    Create Settings.dart

---
+++ vector/lib/pages/Settings.dart
@@ -0,0 +1,24 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-07-30 18:40:52
+ * @LastEditTime: 2020-07-30 18:53:56
+ * @LastEditors: Akko
+ * @Description: home
+ * @FilePath: /vector/lib/pages/Home copy 2.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Text('Settings'),
+      ),
+    );
+  }
+}
