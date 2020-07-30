82acd9e5045d9ba20cda6ab7a17e44a5210da667
Akko <33564554+LvFinch@users.noreply.github.com>
2020-07-30 19:02:15 +0800

    Create Date.dart

---
+++ vector/lib/pages/Date.dart
@@ -0,0 +1,24 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-07-30 18:40:52
+ * @LastEditTime: 2020-07-30 18:53:34
+ * @LastEditors: Akko
+ * @Description: home
+ * @FilePath: /vector/lib/pages/Home copy.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+class Date extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Date'),
+      ),
+      body: Center(
+        child: Text('Date'),
+      ),
+    );
+  }
+}
