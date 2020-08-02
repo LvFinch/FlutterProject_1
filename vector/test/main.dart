634fad46cf79cf3eb4c12dc6015fcbba7d4195b8
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 23:32:05 +0800

    Create main.dart

---
+++ vector/test/main.dart
@@ -0,0 +1,26 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 19:15:32
+ * @LastEditTime: 2020-08-02 23:22:55
+ * @LastEditors: Akko
+ * @Description: 
+ * @FilePath: /vector/test/main.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(new MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Vector',
+//       theme: ThemeData.light(),
+//       routes: {
+//         "/": (context) => MyHomePage(), //注册首页路由
+//         // "new_page": (context) => NewRoute(),
+//       },
+//     );
+//   }
+// }
