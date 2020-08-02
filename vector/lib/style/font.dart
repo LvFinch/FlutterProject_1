1446b723641c4fd34ff04ee4b488a1d0f0cecf8d
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 16:09:35 +0800

    Create font.dart

---
+++ vector/lib/style/font.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 16:07:24
+ * @LastEditTime: 2020-08-02 16:07:25
+ * @LastEditors: Akko
+ * @Description: 字体风格
+ * @FilePath: /vector/lib/style/font.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
