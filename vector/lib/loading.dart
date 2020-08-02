44ccf400540e774cc6adcabc6ead55bf0b321aff
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 23:31:46 +0800

    Create loading.dart

---
+++ vector/lib/loading.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 16:12:02
+ * @LastEditTime: 2020-08-02 16:12:20
+ * @LastEditors: Akko
+ * @Description: 启动页
+ * @FilePath: /vector/lib/loading.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
