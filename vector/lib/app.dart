38c81fc1453b159e8983632b6224caad73c1ca8b
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 23:31:43 +0800

    Create app.dart

---
+++ vector/lib/app.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 16:12:35
+ * @LastEditTime: 2020-08-02 16:12:49
+ * @LastEditors: Akko
+ * @Description: 工程主文件
+ * @FilePath: /vector/lib/app.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
