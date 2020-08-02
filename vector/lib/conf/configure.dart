0a909612507d4c27b34babc81386f95bda5e61ac
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 16:09:10 +0800

    Create configure.dart

---
+++ vector/lib/conf/configure.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 16:05:45
+ * @LastEditTime: 2020-08-02 16:06:16
+ * @LastEditors: Akko
+ * @Description: 配置
+ * @FilePath: /vector/lib/conf/configure.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
