878761101ff9ba83a66aea9c2de3c688303f0d59
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 23:31:52 +0800

    Create homeEvent.dart

---
+++ vector/lib/pages/homeEvent.dart
@@ -0,0 +1,19 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 21:14:38
+ * @LastEditTime: 2020-08-02 23:24:43
+ * @LastEditors: Akko
+ * @Description: 首页Event
+ * @FilePath: /vector/lib/pages/homeEvent.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+class homeEvent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('event'),
+    );
+  }
+}
