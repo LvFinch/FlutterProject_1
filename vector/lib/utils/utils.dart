2df4882022d3935f44c56a5f92c31aceb2d86601
Akko <33564554+LvFinch@users.noreply.github.com>
2020-08-02 16:09:41 +0800

    Create utils.dart

---
+++ vector/lib/utils/utils.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: Akko
+ * @Date: 2020-08-02 16:08:18
+ * @LastEditTime: 2020-08-02 16:08:19
+ * @LastEditors: Akko
+ * @Description: 工具
+ * @FilePath: /vector/lib/utils/utils.dart
+ * @ Copyright ©️ 2020 Akko All rights reserved.
+ */
